21a8518e93b60eabea3121e310f642d15c6cd0fa
sabadia <saabadia@estudiante.uniajc.edu.co>
2026-03-01 23:16:19 -0500

    Añadiendo el nuevo formulario para el proyecto

---
+++ js/formularioProyecto.js
@@ -0,0 +1,27 @@
+// attach listener to form now that it has an id
+const form = document.getElementById("productoForm");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let nombre = document.getElementById("nombre").value.trim();
+  let mensaje = document.getElementById("mensaje").value.trim();
+  let tratamientoDatos = document.getElementById("tratamientoDatos").checked;
+
+  if (!nombre || !mensaje) {
+    alert("Por favor, completa todos los campos requeridos.");
+    return;
+  }
+
+  if (!tratamientoDatos) {
+    alert("Por favor, acepta el tratamiento de datos.");
+    return;
+  }
+
+  let proyectos = JSON.parse(localStorage.getItem("proyectos")) || [];
+  proyectos.push({ nombre, mensaje, tratamientoDatos });
+  localStorage.setItem("proyectos", JSON.stringify(proyectos));
+
+  alert("Proyecto registrado correctamente.");
+
+  location.reload();
+});
